f7c552ffeb94c8c631d24eeac592d797c4d2da71
Matheus Gomes <matheus.lima.gomes@outlook.com>
2021-04-01 22:52:15 -0300

    setup dioProvider

--- lib/infrastructure/provider/api_provider.dart
+++ lib/infrastructure/provider/api_provider.dart
@@ -1,1 +1,14 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_config/flutter_config.dart';
+
+const kServerError = 'Failed to connect to the server. Try again Later.';
+final apiKey = FlutterConfig.get('API_KEY');
+final baseUrl = FlutterConfig.get('BASE_URL');
+
+final kDioOptions = BaseOptions(
+  baseUrl: baseUrl,
+  connectTimeout: 5000,
+  receiveTimeout: 3000,
+  contentType: 'application/json;charset=utf-8',
+  headers: {'Authorization': 'Bearer $apiKey'},
+);
